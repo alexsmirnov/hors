6aa95e6aae7237b9369bc08073722cb23e0f2c35
Alexander Smirnov <asmirnov@asmirnov.hq.exadel.com>
2011-03-08 16:51:02 -0800

    add api and tck projects

---
+++ api/src/main/java/org/hors/servlet/HorsServlet.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.hors.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author asmirnov
+ *
+ */
+public class HorsServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7612042472716417975L;
+
+}
